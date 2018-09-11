a21440d95082b3cb56fb5b5bcb982944bb50a8cd
hpetri1 <39070807+hpetri1@users.noreply.github.com>
2018-09-10 20:38:51 -0600

    Created new Web Project

---
+++ ChinookCS/Chinook.Data/Entities/Album.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(160, ErrorMessage = "Title is limited to 160 characters")]
+        public string Title { get; set; }
+
+        //[Required(ErrorMessage = "Artist ID is required.")] 2.) don't need the validation here as well
+        public int ArtistId { get; set; }
+
+        //[Required(ErrorMessage = "Release Year is required.")] 1.) validation here is useless, because the default value of an int is 0, so the field will always have a value in it
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "Release Label is limited to 50 characters")]
+        public string ReleaseLabel { get; set; }
+    }
+}
---
+++ ChinookCS/Chinook.Data/Entities/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    {
+        [Key] // or [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity) but don't really need it, it is optional]; For Compound keys: [Key, Column(Order = 1)] for the first one, [Key, Column(Order =2)] for the second one, and so on...
+        public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(120, ErrorMessage = "Name is limited to 120 characters")]
+        public string Name { get; set; }
+    }
+}
